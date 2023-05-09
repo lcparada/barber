0b120cb42b4c337ea22a3cb44de2c86f937e02d0
lcparada <loparada@gmail.com>
2023-05-09 18:21:15 -0300

    feat: create firebaseconfig

---
+++ src/config/firebaseconfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAhAOzMn9Mp0wzWJ-c6fFW8SV_dKUdxI8Q",
+  authDomain: "devsbarber-a4ef9.firebaseapp.com",
+  projectId: "devsbarber-a4ef9",
+  storageBucket: "devsbarber-a4ef9.appspot.com",
+  messagingSenderId: "895080800245",
+  appId: "1:895080800245:web:16ce950da8188a214e8abc",
+};
