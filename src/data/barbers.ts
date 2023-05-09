40b640a5b47d8b297a8b0adeedcbe50a33636e2e
lcparada <loparada@gmail.com>
2023-05-09 18:21:52 -0300

    feat: create data of barbers

---
+++ src/data/barbers.ts
@@ -0,0 +1,158 @@
+export const barbers = [
+  {
+    id: 1,
+    name: "Beatriz Farias",
+    avatar: "https://i.pravatar.cc/150?img=31",
+    services: [
+      {
+        nameService: "Corte de cabelo masculino",
+        price: "25,00",
+      },
+      {
+        nameService: "Corte de cabelo feminino",
+        price: "35,00",
+      },
+      {
+        nameService: "Corte de cabelo infantil",
+        price: "20,00",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Julia Santos",
+    avatar: "https://i.pravatar.cc/150?img=32",
+    services: [
+      {
+        nameService: "Corte de cabelo masculino",
+        price: "25,00",
+      },
+      {
+        nameService: "Corte de cabelo feminino",
+        price: "35,00",
+      },
+      {
+        nameService: "Corte de cabelo infantil",
+        price: "20,00",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Pedro Mendonça",
+    avatar: "https://i.pravatar.cc/150?img=33",
+    services: [
+      {
+        nameService: "Corte de cabelo masculino",
+        price: "25,00",
+      },
+      {
+        nameService: "Corte de cabelo feminino",
+        price: "35,00",
+      },
+      {
+        nameService: "Corte de cabelo infantil",
+        price: "20,00",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Julia Shinoda",
+    avatar: "https://i.pravatar.cc/150?img=34",
+    services: [
+      {
+        nameService: "Corte de cabelo masculino",
+        price: "25,00",
+      },
+      {
+        nameService: "Corte de cabelo feminino",
+        price: "35,00",
+      },
+      {
+        nameService: "Corte de cabelo infantil",
+        price: "20,00",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Andrezza Shinoda",
+    avatar: "https://i.pravatar.cc/150?img=35",
+    services: [
+      {
+        nameService: "Corte de cabelo masculino",
+        price: "25,00",
+      },
+      {
+        nameService: "Corte de cabelo feminino",
+        price: "35,00",
+      },
+      {
+        nameService: "Corte de cabelo infantil",
+        price: "20,00",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Sara Maria",
+    avatar: "https://i.pravatar.cc/150?img=36",
+    services: [
+      {
+        nameService: "Corte de cabelo masculino",
+        price: "25,00",
+      },
+      {
+        nameService: "Corte de cabelo feminino",
+        price: "35,00",
+      },
+      {
+        nameService: "Corte de cabelo infantil",
+        price: "20,00",
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "Edna Sousa",
+    avatar: "https://i.pravatar.cc/150?img=37",
+    services: [
+      {
+        nameService: "Corte de cabelo masculino",
+        price: "25,00",
+      },
+      {
+        nameService: "Corte de cabelo feminino",
+        price: "35,00",
+      },
+      {
+        nameService: "Corte de cabelo infantil",
+        price: "20,00",
+      },
+    ],
+  },
+];
+
+export const assessments = [
+  {
+    id: "1",
+    name: "Lucas Farias",
+    comment: "Ótimo atendimento, recomendo!",
+  },
+  {
+    id: "2",
+    name: "Claudio Santos",
+    comment: "Amei o corte!",
+  },
+  {
+    id: "3",
+    name: "Pedro Mendonça",
+    comment: "Voltarei sempre",
+  },
+  {
+    id: "4",
+    name: "Lucas Farias",
+    comment: "Muito atencioso, bom demais!",
+  },
+];
