d503a99ffde99dbba31347646922cf5d2e23f072
lcparada <loparada@gmail.com>
2023-05-09 18:21:36 -0300

    chore: create types of navigation

---
+++ @types/navigation.d.ts
@@ -0,0 +1,17 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Login: undefined;
+      Register: undefined;
+      Home: undefined;
+      AuthRoutes: undefined;
+      AppRoutes: undefined;
+      Profile: undefined;
+      Favorites: undefined;
+      Search: undefined;
+      Information: undefined;
+      ProfileRoutes: undefined;
+      EditProfile: undefined;
+    }
+  }
+}
